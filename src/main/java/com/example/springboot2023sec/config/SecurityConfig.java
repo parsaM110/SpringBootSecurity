eba20243d698d8bc74ad693c5f46e9ef06a034b0
parsaM110 <pmomeni80@gmail.com>
2023-10-18 18:28:19 +0330

    adding Basic Auth but you have to add @Configuration otherwise it won't work/ adding Help.md file

---
+++ src/main/java/com/example/springboot2023sec/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.springboot2023sec.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+//    @Bean
+//    @Order(2147483642)
+//    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests((requests) -> {
+//            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated();
+//        });
+//        http.formLogin(Customizer.withDefaults());
+//        http.httpBasic(Customizer.withDefaults());
+//        return (SecurityFilterChain)http.build();
+//    }
+    @Bean
+//    @Order(SecurityProperties.BASIC_AUTH_ORDER) // we are overwriting this shit
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic(withDefaults());
+//        http.formLogin(Customizer.withDefaults());
+//        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
