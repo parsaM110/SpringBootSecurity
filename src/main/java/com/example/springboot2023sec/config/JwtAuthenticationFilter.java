240e0c6ab534b84e716802c537184a2e0b64e2b4
parsaM110 <pmomeni80@gmail.com>
2023-10-21 01:53:06 +0330

    adding JwtAuthenticationFilter but seems @NotNull is deprecated too

---
+++ src/main/java/com/example/springboot2023sec/config/JwtAuthenticationFilter.java
@@ -0,0 +1,26 @@
+package com.example.springboot2023sec.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            @NotNull HttpServletRequest request,
+            @NotNull HttpServletResponse response,
+            @NotNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+    }
+}
