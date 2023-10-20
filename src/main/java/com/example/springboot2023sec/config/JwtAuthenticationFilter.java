d012981e9dfb96594b6e7b12962f6341f0fae771
parsaM110 <pmomeni80@gmail.com>
2023-10-21 01:56:26 +0330

    implementing jwt extraction

--- src/main/java/com/example/springboot2023sec/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/springboot2023sec/config/JwtAuthenticationFilter.java
@@ -22,5 +22,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NotNull FilterChain filterChain
     ) throws ServletException, IOException {
 
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        if(authHeader != null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+
+
+
     }
 }
