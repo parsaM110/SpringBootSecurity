99b8d408e5b41539fc72705e3abc47d4aadd1df9
parsaM110 <pmomeni80@gmail.com>
2023-10-21 01:37:40 +0330

    adding jpa and postgres driver and making the User entity using lombok and jpa

---
+++ src/main/java/com/example/springboot2023sec/user/User.java
@@ -0,0 +1,26 @@
+package com.example.springboot2023sec.user;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table (name = "_user")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+}
