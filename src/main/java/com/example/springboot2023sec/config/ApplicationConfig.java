b367e5050594ef3ef4f7f0c467a299bdaf3eb2f0
parsaM110 <pmomeni80@gmail.com>
2023-10-21 12:26:38 +0330

    implementing and overWriting UserDetailsService with lambda style

---
+++ src/main/java/com/example/springboot2023sec/config/ApplicationConfig.java
@@ -0,0 +1,23 @@
+package com.example.springboot2023sec.config;
+
+import com.example.springboot2023sec.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+}
