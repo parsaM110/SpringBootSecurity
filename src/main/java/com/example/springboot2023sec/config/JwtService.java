c3b1a9568d86e524fbb1948a8ee6877cf92c51f6
parsaM110 <pmomeni80@gmail.com>
2023-10-21 12:04:11 +0330

    adding generateToken for both userDetails and with/without extraClaims

--- src/main/java/com/example/springboot2023sec/config/JwtService.java
+++ src/main/java/com/example/springboot2023sec/config/JwtService.java
@@ -2,12 +2,17 @@ package com.example.springboot2023sec.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.cglib.core.internal.Function;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JwtService {
@@ -21,6 +26,24 @@ public class JwtService {
     public <T> T extractClaim(String token, Function<Claims,T> claimsResolver){
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(),userDetails);
+    }
+
+    public String generateToken(
+            Map<String ,Object> extraClaims,
+            UserDetails userDetails
+    ){
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.ES256)
+                .compact();
     }
 
     private Claims extractAllClaims(String token){
