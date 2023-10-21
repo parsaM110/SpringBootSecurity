ae9488a94194f8db35a99661e273d0882a3f223d
parsaM110 <pmomeni80@gmail.com>
2023-10-21 11:39:10 +0330

    add some code JwtService but still there is error getSignInKey is not implemented

--- src/main/java/com/example/springboot2023sec/config/JwtService.java
+++ src/main/java/com/example/springboot2023sec/config/JwtService.java
@@ -1,10 +1,22 @@
 package com.example.springboot2023sec.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
+
     public String extractUsername(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
