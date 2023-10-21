afa124280be80475ff0cc58b5530750c2cf8b700
parsaM110 <pmomeni80@gmail.com>
2023-10-21 11:52:30 +0330

    implementing getSignInKey

--- src/main/java/com/example/springboot2023sec/config/JwtService.java
+++ src/main/java/com/example/springboot2023sec/config/JwtService.java
@@ -2,10 +2,16 @@ package com.example.springboot2023sec.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "54CF4A0AF7872773861E251C5E1DA966DE4D36E0F7A052A9A7892C2152C61229";
 
     public String extractUsername(String token) {
         return null;
@@ -18,5 +24,10 @@ public class JwtService {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
