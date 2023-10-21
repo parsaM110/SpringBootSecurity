c5f217c5ba2692f968712832fa8c3facd94ac032
parsaM110 <pmomeni80@gmail.com>
2023-10-21 12:15:19 +0330

    progressing in main JwtAuthenticationFilter class for jwt authentication

--- src/main/java/com/example/springboot2023sec/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/springboot2023sec/config/JwtAuthenticationFilter.java
@@ -6,6 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,6 +19,7 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService ;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(
@@ -33,6 +37,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwt);
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+
+        }
 
 
 
