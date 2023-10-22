f892ff3d3f7e7a1880a28e48f58826e18331314b
parsaM110 <pmomeni80@gmail.com>
2023-10-22 14:44:27 +0330

    implementing AuthenticationService.register

--- src/main/java/com/example/springboot2023sec/auth/AuthenticationService.java
+++ src/main/java/com/example/springboot2023sec/auth/AuthenticationService.java
@@ -1,13 +1,33 @@
 package com.example.springboot2023sec.auth;
 
+import com.example.springboot2023sec.config.JwtService;
+import com.example.springboot2023sec.user.Role;
+import com.example.springboot2023sec.user.User;
+import com.example.springboot2023sec.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
