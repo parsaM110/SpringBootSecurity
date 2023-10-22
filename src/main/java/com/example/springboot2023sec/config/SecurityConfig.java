9b97533fd983a747b3f705b8aa9c0ede0b5675a3
parsaM110 <pmomeni80@gmail.com>
2023-10-22 13:49:42 +0330

    changing SecurityConfig to Implement JWT but the problem is it has updated again I have add withDefaults whenever I get an error

--- src/main/java/com/example/springboot2023sec/config/SecurityConfig.java
+++ src/main/java/com/example/springboot2023sec/config/SecurityConfig.java
@@ -1,22 +1,31 @@
 package com.example.springboot2023sec.config;
 
+import jakarta.servlet.Filter;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-//    @Bean
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    //    @Bean
 //    @Order(2147483642)
 //    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 //        http.authorizeHttpRequests((requests) -> {
@@ -30,13 +39,11 @@ public class SecurityConfig {
 //    @Order(SecurityProperties.BASIC_AUTH_ORDER) // we are overwriting this shit
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic(withDefaults());
-//        http.formLogin(Customizer.withDefaults());
-//        http.httpBasic(Customizer.withDefaults());
+                .csrf(withDefaults())
+                .authorizeHttpRequests(withDefaults())
+                .sessionManagement(withDefaults())
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
