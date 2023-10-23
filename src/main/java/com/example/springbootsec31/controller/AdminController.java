a178fedffe52d8db2ed5c6e4ad31449135825007
parsaM110 <pmomeni80@gmail.com>
2023-10-23 07:55:03 +0330

    creating controllers

---
+++ src/main/java/com/example/springbootsec31/controller/AdminController.java
@@ -0,0 +1,11 @@
+package com.example.springbootsec31.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/v1/admin")
+@RequiredArgsConstructor
+public class AdminController {
+}
---
+++ src/main/java/com/example/springbootsec31/controller/AuthenticationController.java
@@ -0,0 +1,11 @@
+package com.example.springbootsec31.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+}
