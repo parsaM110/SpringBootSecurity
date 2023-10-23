36bfb53a6635c6142898d3ea6ab13c86f20576ce
parsaM110 <pmomeni80@gmail.com>
2023-10-23 10:43:07 +0330

    implementing /signup endpoint in AuthenticationController

--- src/main/java/com/example/springbootsec31/controller/AuthenticationController.java
+++ src/main/java/com/example/springbootsec31/controller/AuthenticationController.java
@@ -1,11 +1,24 @@
 package com.example.springbootsec31.controller;
 
+import com.example.springbootsec31.dto.SignUpRequest;
+import com.example.springbootsec31.entity.User;
+import com.example.springbootsec31.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
+
+    private AuthenticationService authenticationService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> signup(@RequestBody SignUpRequest signUpRequest){
+        return ResponseEntity.ok(authenticationService.signup(signUpRequest));
+    }
 }
